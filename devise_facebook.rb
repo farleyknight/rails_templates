1267dce2d1971f07e02a5e458d81aee13959db0d
Farley Knight <farleyknight@gmail.com>
2013-09-03 19:24:29 -0400

    Add more_devise.rb and devise_facebook.rb

--- devise_facebook.rb
+++ devise_facebook.rb
@@ -1,8 +1,13 @@
 
+
 gem "devise"
+gem "omniauth"
+gem "omniauth-facebook"
 
 generate "devise:install"
 generate "devise", "User"
 
 rake "db:migrate"
+
+generate "devise:views", "User"
 
--- more_devise.rb
+++ more_devise.rb
@@ -1,8 +1,11 @@
 
+
 gem "devise"
 
 generate "devise:install"
 generate "devise", "User"
 
 rake "db:migrate"
+
+generate "devise:views", "User"
 
