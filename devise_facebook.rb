6b7d9520795095a12466bd5ad1dc745049836ec4
Farley Knight <farleyknight@gmail.com>
2013-09-08 06:02:36 -0400

    Update devise_facebook.rb

--- devise_facebook.rb
+++ devise_facebook.rb
@@ -1,5 +1,9 @@
 require 'open-uri'
 
+FILES = {
+  controller: "https://raw.github.com/farleyknight/rails_templates/master/devise_facebook/omniauth_controller.rb"
+}
+
 gem "devise"
 gem "omniauth"
 gem "omniauth-facebook"
@@ -10,16 +14,10 @@ generate "devise:install"
 generate "devise", "User"
 generate "devise:views"
 
-# TODO: Move this to a separate file
-
-files = {
-  controller: "url_for_omniauth_controller"
-}
-
-file "app/controllers/users/omniauth_callbacks_controller.rb", open(files[:controller]).read
+file "app/controllers/users/omniauth_callbacks_controller.rb", open(FILES[:controller]).read
 
 insert_into_file "config/initializers/devise.rb", "\n  config.omniauth :facebook, 'APP_ID', 'APP_SECRET'", before: "\nend"
 
-rake "db:migrate"
-
 gsub_file "config/routes.rb", "devise_for :users", 'devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }'
+
+rake "db:migrate"
