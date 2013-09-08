2a369ef72b15a61e736267910d585c2606297059
Farley Knight <farleyknight@gmail.com>
2013-09-08 05:56:53 -0400

    Update devise_facebook.rb

--- devise_facebook.rb
+++ devise_facebook.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 gem "devise"
 gem "omniauth"
 gem "omniauth-facebook"
@@ -6,26 +8,15 @@ run "bundle install"
 
 generate "devise:install"
 generate "devise", "User"
-generate "devise:views", "User"
+generate "devise:views"
 
 # TODO: Move this to a separate file
 
-file "app/controllers/users/omniauth_callbacks_controller.rb", <<-CODE
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+files = {
+  controller: "url_for_omniauth_controller"
+}
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
-end
-CODE
+file "app/controllers/users/omniauth_callbacks_controller.rb", open(files[:controller]).read
 
 insert_into_file "config/initializers/devise.rb", "\n  config.omniauth :facebook, 'APP_ID', 'APP_SECRET'", before: "\nend"
 
